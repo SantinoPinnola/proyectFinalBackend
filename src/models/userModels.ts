605542627de9cb70595fee7fd3adcd68a12a0d1e
SantinoPinnola <santinopinnola9@gmail.com>
2021-11-10 20:59:37 -0300

    commit

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -1,27 +1,95 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import { NewUserI, UserI, UserBaseClass, UserQuery } from '../interfaces/usersInterfaces';
+import config from '../config/index';
 
-const userCollection = 'users';
-
-const UserSchema = new mongoose.Schema({
-    email : {type : String, require : true},
-    username : {type : String, require : true},
-    password : {type : String, require : true}
+const usersSchema = new mongoose.Schema<UserI>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
 });
 
+usersSchema.pre('save', async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(user.password, 10);
 
-UserSchema.pre('save',  async function (next) {
-    const user = this;
-    const hash = await bcrypt.hash(user.password, 10);
-  
-    this.password = hash;
-    next();
+  this.password = hash;
+  next();
 });
 
-UserSchema.methods.isValidPassword = async function (password) {
-    const user = this;
+export class UsersAtlas implements UserBaseClass {
+  private users;
+
+  constructor() {
+    this.users = mongoose.model<UserI>('user', usersSchema);
+  }
+
+  async get(id?: string): Promise<UserI[]> {
+    let output: UserI[] = [];
+    try {
+      if (id) {
+        const document = await this.users.findById(id);
+        if (document) output.push(document);
+      } else {
+        output = await this.users.find();
+      }
+
+      return output;
+    } catch (err) {
+      return output;
+    }
+  }
+
+  async add(data: NewUserI): Promise<UserI> {
+    const newProduct = new this.users(data);
+    await newProduct.save();
+
+    return newProduct;
+  }
+
+  async update(id: string, data: NewUserI): Promise<UserI> {
+    return this.users.findByIdAndUpdate(id, data);
+  }
+
+  async delete(id: string) {
+    await this.users.findByIdAndDelete(id);
+  }
+
+  async query(query: any): Promise<UserI> {
+    const result = await this.users.find(query);
+    return result[0];
+  }
+
+  async validateUserPassword(
+    username: string,
+    password: string
+  ): Promise<boolean> {
+    const user = await this.users.findOne({ username });
+
+    if (!user) return false;
+
     const compare = await bcrypt.compare(password, user.password);
-    return compare;
-  };
-  
-  export const UserModel = mongoose.model('user', UserSchema);
+
+    if (!compare) return false;
+    return true;
+  }
+}
